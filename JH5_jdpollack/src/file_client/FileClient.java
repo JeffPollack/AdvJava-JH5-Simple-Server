d632c965fc8c7ed764b84e143f0a8d7bfde49fa8
Jeff Pollack <JeffPollack@users.noreply.github.com>
2017-03-31 10:01:12 -0400

    Add files via upload

---
+++ JH5_jdpollack/src/file_client/FileClient.java
@@ -0,0 +1,50 @@
+package file_client;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class FileClient extends EchoClient {
+
+
+	public void communicate (InputStream is, OutputStream os){
+		Scanner scanSocket = new Scanner(is);
+
+		PrintStream psSocket = new PrintStream(os, true);
+
+		Scanner keyboard = new Scanner(System.in);
+
+		System.out.println("Start entering text to FileServer ....");
+		String fromKeyboard= keyboard.nextLine();
+		do 
+		{
+			psSocket.println(fromKeyboard); // write to socket
+						
+			while (scanSocket.hasNextLine())
+			{
+				String sFromSocket = scanSocket.nextLine(); // read from socket
+				if (sFromSocket.length() == 0)
+					break;
+				System.out.println("received from server: "+sFromSocket);
+			}
+			
+			fromKeyboard = keyboard.nextLine();
+		}	while (!fromKeyboard.equals("quit"));
+			scanSocket.close();
+			psSocket.close();
+			keyboard.close();
+			System.out.println("Exitting communicate");
+		
+		
+	}
+	public static void main(String[] args) {
+
+
+		FileClient fc= new FileClient();
+		fc.startup(args);
+		System.out.println("Exitting main");
+
+	}
+
+}
---
+++ JH5_jdpollack/src/file_server/ConnectListener.java
@@ -0,0 +1,8 @@
+package file_server;
+
+import java.net.Socket;
+
+public interface ConnectListener {
+    public void handleConnect(Socket s);
+
+}
